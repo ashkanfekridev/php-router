2e591cdfdedd9e9e99fd7df71462cf0022bedddd
ashkanfekridev <ashkanfekridev@gmail.com>
2020-07-17 14:58:28 +0430

    fix a bug

--- src/Router.php
+++ src/Router.php
@@ -55,7 +55,7 @@ class Router
         }
     }
 
-    ppublic function run()
+    public function run()
     {
         $requestUrl = (string) urldecode(Request::url());
         $requestMethod = Request::method();
